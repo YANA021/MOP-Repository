f2df6fb76991a4f292b0225804f883b8cdbace2e
Yahir Nieto <94183916+YANA021@users.noreply.github.com>
2025-06-27 00:51:04 -0600

    Agregando db postgres de railway y protegiendo la url para mas seguridad

--- Mop_Proyecto/settings.py
+++ Mop_Proyecto/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,14 +79,8 @@ WSGI_APPLICATION = 'Mop_Proyecto.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Metodo_Grafico',
-        'USER': 'postgres',
-        'PASSWORD': '231409',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+        
 }
 
 
