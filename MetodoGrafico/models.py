6c024f4f3a481fd9d01d3fd61ce23e4562994b0b
Yahir Nieto <94183916+YANA021@users.noreply.github.com>
2025-06-27 16:01:09 -0600

    Agregando el modelo que ocuparemos en el metodo grafico

--- MetodoGrafico/models.py
+++ MetodoGrafico/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class ProblemaPL(models.Model):
+    OBJETIVO_CHOICES = [
+        ('max', 'Maximizar'),
+        ('min', 'Minimizar'),
+    ]
+
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name='problemas_pl',
+        null=True, blank=True
+    )
+    objetivo = models.CharField(max_length=3, choices=OBJETIVO_CHOICES)
+    coef_x1 = models.FloatField()
+    coef_x2 = models.FloatField()
+    restricciones = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"PL {self.get_objetivo_display()}"
