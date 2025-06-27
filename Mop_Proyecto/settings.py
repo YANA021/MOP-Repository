18c8cc8561ada912ddb1e2830c83dec187ed5adc
Yahir Nieto <94183916+YANA021@users.noreply.github.com>
2025-06-26 22:45:53 -0600

    hope

--- Mop_Proyecto/settings.py
+++ Mop_Proyecto/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ["mop-repository-production.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
